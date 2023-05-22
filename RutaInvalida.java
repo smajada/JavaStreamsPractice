5ec509641ea0f42fbf3614f80f9b4672679f1770
Sergio Majada <smajadamanresa@cifpfbmoll.eu>
2023-05-22 11:22:19 +0200

    Updated timestamp and removed salida.txt

--- RutaInvalida.java
+++ RutaInvalida.java
@@ -1,9 +1,11 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.Date;
 
 public class RutaInvalida extends Exception {
+    private String timestamp = 
+    new java.text.SimpleDateFormat("MM/dd/yyyy h:mm:ss a").format(new Date());
 
     public RutaInvalida(){
         
@@ -16,7 +18,7 @@ public class RutaInvalida extends Exception {
     public void recordLogs() {
         try {
             try (FileWriter logFile = new FileWriter("./logs_error.txt")) {
-                logFile.write(super.getMessage()+ "\n" + LocalDate.now() + "\n" + Arrays.toString(this.getStackTrace()) + "\n");
+                logFile.write(super.getMessage()+ "\n" + timestamp + "\n" + Arrays.toString(this.getStackTrace()) + "\n");
             }
         } catch (IOException e) {
             System.err.println("Error abriendo logs_error.txt");
